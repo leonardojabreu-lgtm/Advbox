6cfea11f2c6b5a33890a0224b23782c3a57cb3a4
leonardojabreu-lgtm <leonardojabreu@gmail.com>
2025-11-20 18:41:01 -0300

    Update supabaseClient.js

--- app/internal/supabaseClient.js
+++ app/internal/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("❌ Variáveis SUPABASE_URL ou SUPABASE_ANON_KEY não foram definidas!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
